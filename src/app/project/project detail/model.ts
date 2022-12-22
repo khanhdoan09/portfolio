5003fb3a846c421f7ab097d53f3efbe7d23e8a25
Đoàn Trần Phi Khánh <80077540+khanhdoan09@users.noreply.github.com>
2022-12-22 16:26:10 +0700

    update new project

--- src/app/project/project detail/model.ts
+++ src/app/project/project detail/model.ts
@@ -2,7 +2,7 @@ export let type = {group: 'group project', personal: 'personal project'}
 export let tag = {
   all: 'showAll', html:'html', css:'css', js:'javascript',
    java:'java', cs:'c#', angular:'angular', nodejs:'nodejs',
-    mysql:'mysql', sqlserver:'sql server'}
+    mysql:'mysql', sqlserver:'sql server', mongodb: 'mongodb', react: 'react'}
 let logo={
 html:'assets/image/logo/html.svg',
 css:'assets/image/logo/css.svg',
@@ -19,7 +19,9 @@ dotNet:'assets/image/logo/net.svg',
 sqlServer:'assets/image/logo/sql-server.svg',
 mySql:'assets/image/logo/mysql.svg',
 flutter:'assets/image/logo/flutter.svg',
-spring:'https://cdn.freebiesupply.com/logos/large/2x/spring-3-logo-png-transparent.png'}
+mongodb: 'https://w7.pngwing.com/pngs/956/695/png-transparent-mongodb-original-wordmark-logo-icon-thumbnail.png',
+spring:'https://cdn.freebiesupply.com/logos/large/2x/spring-3-logo-png-transparent.png',
+react: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png'}
 
 export interface Project {
   id: string
@@ -28,15 +30,17 @@ export interface Project {
   logo: string[]
   type: string
   content: string
-  source: string
+  source_1: string
+  source_2: string
 }
 
 export let projects:Project[]= [
-  {id: '1', tag:[tag.all, tag.html, tag.css, tag.js, tag.angular], name:'portfolio', logo:[logo.angular, logo.html, logo.css, logo.js, logo.bootstrap], type:type.personal, content:'Not use web template to introduce information about me, what skills i have and technology i have used', source:'#'},
-  {id: '2', tag:[tag.all, tag.cs, tag.sqlserver], name:'admin page', logo:[logo.cs, logo.dotNet, logo.sqlServer], type:type.group, content:' I write backend amin page which applied asp mvc, linQ, entity framework', source:'https://github.com/khanhdoan09/Admin-Page'},
-  {id:'3', tag:[tag.all, tag.java], name:'web stream movie', logo:[logo.spring, logo.java, logo.mySql], type:type.personal, content:'I code backend for a website to watch movie online with spring boot', source:'https://github.com/khanhdoan09/movie_be'},
-  {id: '4', tag:[tag.all, tag.java, tag.mysql], name:'shoe e-commerce', logo:[logo.java, logo.jsp, logo.jdbc,logo.jquery, logo.mySql], type:type.group, content:'I write backend for amin page, filter search, pagination which applied jsp and servlet', source:'https://github.com/khanhdoan09/ltw'},
-  {id: '5', tag:[tag.all, tag.java], name:'tetris game', logo:[logo.java], type:type.group, content:'I used design pattern(strategy pattern, observer pattern, singleton pattern) to translate language for this project', source:'https://github.com/khanhdoan09/TKHDT'},
-  {id:'6', tag:[tag.all, tag.html, tag.css, tag.js], name:'pokemon game', logo:[logo.html, logo.css, logo.js], type:type.personal, content:'Create a game pokemon connection with vanilla js and scss', source:'https://github.com/khanhdoan09/pikachu_game'},
-  {id:'7', tag:[tag.all, tag.html, tag.css, tag.js, tag.angular], name:'app chat', logo:[logo.html, logo.css, logo.js, logo.angular], type:type.group, content:'use api firebase and api school to take on the task of sending and downloading messages in project', source:'https://github.com/khanhdoan09/Lap_Trinh_Frontend_Nhom31'},
+  {id: '1', tag:[tag.all, tag.html, tag.css, tag.js, tag.angular], name:'portfolio', logo:[logo.angular, logo.html, logo.css, logo.js, logo.bootstrap], type:type.personal, content:'Not use web template to introduce information about me, what skills i have and technology i have used', source_1:'#', source_2:''},
+  {id:'2', tag:[tag.all, tag.react, tag.nodejs, tag.mongodb], name:'booking home', logo:[logo.react, logo.nodejs, logo.mongodb], type:type.group, content:'I do the job of filtering searches, updating user information, getting purchase history and evaluating purchased products', source_1:'https://github.com/khanhdoan09/house-booking-server', source_2:'https://github.com/khanhdoan09/house-booking-client'},
+  {id: '3', tag:[tag.all, tag.cs, tag.sqlserver], name:'admin page', logo:[logo.cs, logo.dotNet, logo.sqlServer], type:type.group, content:' I write backend amin page which applied asp mvc, linQ, entity framework', source_1:'https://github.com/khanhdoan09/Admin-Page', source_2:''},
+  {id:'4', tag:[tag.all, tag.java], name:'web stream movie', logo:[logo.spring, logo.java, logo.mySql], type:type.personal, content:'I code backend for a website to watch movie online with spring boot', source_1:'https://github.com/khanhdoan09/movie_be', source_2:''},
+  {id: '5', tag:[tag.all, tag.java, tag.mysql], name:'shoe e-commerce', logo:[logo.java, logo.jsp, logo.jdbc,logo.jquery, logo.mySql], type:type.group, content:'I write backend for amin page, filter search, pagination which applied jsp and servlet', source_1:'https://github.com/khanhdoan09/ltw', source_2:''},
+  {id: '6', tag:[tag.all, tag.java], name:'tetris game', logo:[logo.java], type:type.group, content:'I used design pattern(strategy pattern, observer pattern, singleton pattern) to translate language for this project', source_1:'https://github.com/khanhdoan09/TKHDT', source_2: ''},
+  {id:'7', tag:[tag.all, tag.html, tag.css, tag.js], name:'pokemon game', logo:[logo.html, logo.css, logo.js], type:type.personal, content:'Create a game pokemon connection with vanilla js and scss', source_1:'https://github.com/khanhdoan09/pikachu_game', source_2:''},
+  {id:'8', tag:[tag.all, tag.html, tag.css, tag.js, tag.angular], name:'app chat', logo:[logo.html, logo.css, logo.js, logo.angular], type:type.group, content:'use api firebase and api school to take on the task of sending and downloading messages in project', source_1:'https://github.com/khanhdoan09/Lap_Trinh_Frontend_Nhom31',  source_2:''},
 ]
